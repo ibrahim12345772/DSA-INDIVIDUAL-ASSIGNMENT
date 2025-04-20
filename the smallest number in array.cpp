6de44923c4fdacb4a912aa3bd4a20b4eda31a528
ibrahim dekebo <ibroibro.7208@gmail.com>
2025-04-20 23:40:31 +0300

    the smallest number

--- the smallest number in array.cpp
+++ the smallest number in array.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int findMinimum(int arr[], int n) {
+    int min = arr[0];
+    for (int i = 1; i < n; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int main() {
+    int arr[] = {9, 2, 14, 7, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int minValue = findMinimum(arr, n);
+    cout << "The smallest number in the array is: " << minValue << endl;
+
+    return 0;
+}
 
